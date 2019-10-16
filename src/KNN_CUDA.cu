108257de456fd0bde4eaa51246335dee50c5794c
Kathryn Young <youngk6@vcu.edu>
2019-10-15 19:05:28 -0700

    moving in right direction

--- src/KNN_CUDA.cu
+++ src/KNN_CUDA.cu
@@ -11,11 +11,28 @@
 #include "libarff/arff_data.h"
 using namespace std;
 
-__global__ void KNN(int *predictions, ArffData *dataset, int num_elements, int k_neighbors)
+__global__ void KNN(int *predictions, float *dataset, int *classes, float *distance_calculations, int k_neighbors, int num_elements)
 {
     int tid = blockDim.x * blockIdx.x + threadIdx.x;
-    // dataset, predictions, num_elements, k_neighbors are here...
-    // solve each instance by looking at what is in various arrays at tid
+
+    /*float ** Arr_dist = new float*[num_elements];
+    int ** Arr_classes = new int*[num_elements];
+    for (int i = 0; i < k_neighbors; i++){
+    	Arr_dist[i] = (float *)malloc(k_neighbors * sizeof(float));
+    	Arr_classes[i] = (int *)malloc(k_neighbors * sizeof(int));
+    }*/
+    //for loop to look at other instances in array
+    for(int i = 0; i < num_elements; i++){
+    	if(i == tid) continue;
+
+    	/*for(int k = 0; k < dataset->num_attributes() - 1; k++){ // compute distance between two instances
+    		float diff = dataset->get_instance(tid)->get(k)->operator float() - dataset->get_instance(i)->get(k)->operator float();
+    		// this gets i, gets feature k, looks at distance from other instance j to k
+    		distance_calculations[tid] += diff * diff;
+    	}
+    	distance_calculations[tid] = sqrt(distance_calculations[tid]);*/
+
+    }
 
 }
 
@@ -27,22 +44,56 @@ int main(int argc, char *argv[])
         exit(0);
     }
 
+    // get dataset
     ArffParser parser(argv[1]);
     ArffData *dataset = parser.parse();
+
+    // create 1D array of the data (without classes)
+    int num_elements = dataset->num_instances();
+    int total_attributes = dataset->num_attributes();
+
+    float *h_arr_data = (float *)malloc(num_elements * total_attributes * sizeof(float));
+    int count = 0;
+    for (int i = 0; i < num_elements; i++){
+    	for (int j = 0; j < total_attributes - 1; j++){
+    		h_arr_data[count] = dataset->get_instance(i)->get(j)->operator float();
+    		count++;
+    	}
+    }
+
+    // create 1D array of classes for each instance
+    count = 0;
+    int *h_arr_class_data = (int *)malloc(num_elements * sizeof(int));
+
+    for(int i = 0; i < num_elements; i++){
+    	h_arr_class_data[count] = dataset->get_instance(i)->get(dataset->num_attributes()-1)->operator long int();
+    	count++;
+    }
+
+    // start clock
     struct timespec start, end;
     clock_gettime(CLOCK_MONOTONIC_RAW, &start);
 
-    int num_elements = dataset->num_instances();
-
-    // Allocate host memory
+    // Allocate other host memory
     int *h_predictions = (int *)malloc(num_elements * sizeof(int));
+    float * h_distance_calculations = (float *)calloc(num_elements, sizeof(float));
 
     // Allocate the device input vector A
     int *d_predictions;
+    float *d_distance_calculations;
+    int *d_arr_class_data;
+    float *d_arr_data;
+
     cudaMalloc(&d_predictions, num_elements * sizeof(int));
+    cudaMalloc(&d_distance_calculations, num_elements *sizeof(float));
+    cudaMalloc(&d_arr_class_data, num_elements * sizeof(int));
+    cudaMalloc(&d_arr_data, num_elements * total_attributes * sizeof(float));
 
     // Copy the host input vectors A and B in host memory to the device input vectors in
     cudaMemcpy(d_predictions, h_predictions, num_elements * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_distance_calculations, h_distance_calculations, num_elements *sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_arr_class_data, h_arr_class_data, num_elements * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_arr_data, h_arr_data, num_elements * total_attributes * sizeof(float), cudaMemcpyHostToDevice);
 
     // Launch the Vector Add CUDA Kernel
     int threadsPerBlock = 256;
@@ -52,7 +103,9 @@ int main(int argc, char *argv[])
 
     // set k value (number of neighbors)
     int k = 5;
-    KNN<<<blocksPerGrid, threadsPerBlock>>>(d_predictions, dataset, num_elements, k);
+    KNN<<<blocksPerGrid, threadsPerBlock>>>(d_predictions, d_arr_data, d_arr_class_data, d_distance_calculations, k, num_elements);
+
+    cudaMemcpy(h_predictions, d_predictions, num_elements * sizeof(int), cudaMemcpyDeviceToHost);
 
     /*
 
